c02fbf47a20a2669b395fd32dc90daf36dd55fb4
Gustavo Ribeiro <tavinbr2143@gmail,com>
2026-05-11 17:39:23 -0300

    Ajustes Sequenciais

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from bson import ObjectId
 from app.dependencies import get_current_user
-from .schemas import UpdateProfileSchema
+from .schemas import UpdateUserSchema
 from .service import (
     get_me,
     update_profile,
@@ -30,7 +30,7 @@ async def me(user=Depends(get_current_user)):
 
 @router.patch("/profile")
 async def profile(
-    data: UpdateProfileSchema,
+    data: UpdateUserSchema,
     user=Depends(get_current_user)
 ):
     return await update_profile(user, data)
