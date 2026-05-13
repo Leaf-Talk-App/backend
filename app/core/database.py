d3c76cb9ae8ceac8324c9e5787b3373b4aa62473
Gustavo Ribeiro <tavinbr2143@gmail,com>
2026-05-13 10:40:23 -0300

    Atualização Render

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
-def get_database():
-    client = AsyncIOMotorClient(settings.MONGO_URL)
-    return client[settings.DATABASE_NAME]
+client = AsyncIOMotorClient(
+    settings.MONGO_URL,
+    serverSelectionTimeoutMS=5000
+)
 
-db = get_database()
+db = client[settings.DATABASE_NAME]
+
+def get_database():
+    return db
